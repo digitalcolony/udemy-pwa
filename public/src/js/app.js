fa8da327b5af57a00adf41356dce0b622d47dc92
MAS <digitalcolony@gmail.com>
2018-06-14 12:56:42 -0700

    thru lecture 48

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,15 +1,60 @@
-const deferredPrompt;
+var deferredPrompt;
 
 if ("serviceWorker" in navigator) {
-  console.log("SW available");
-  navigator.serviceWorker.register("/sw.js").then(function() {
-    console.log("service worker registered");
-  });
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function() {
+      console.log("Service worker registered!");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
 window.addEventListener("beforeinstallprompt", function(event) {
-    console.log("beforeinstallprompt fired");
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;
+  console.log("beforeinstallprompt fired");
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
 });
+
+var promise = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    //resolve('This is executed once the timer is done!');
+    reject({ code: 500, message: "An error occurred!" });
+    //console.log('This is executed once the timer is done!');
+  }, 3000);
+});
+
+// promise.then(function(text) {
+//   return text;
+// }, function(err) {
+//   console.log(err.code, err.message)
+// }).then(function(newText) {
+//   console.log(newText);
+// });
+
+promise
+  .then(function(text) {
+    return text;
+  })
+  .then(function(newText) {
+    console.log(newText);
+  })
+  .catch(function(err) {
+    console.log(err.code, err.message);
+  });
+
+console.log("This is executed right after setTimeout()");
+
+fetch("https://httpbin.org/ip")
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
