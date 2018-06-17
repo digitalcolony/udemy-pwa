250aed79578bb30566d3625cab0d77ff70d419b0
MAS <digitalcolony@gmail.com>
2018-06-17 16:34:02 -0700

    up to 60

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,38 +1,51 @@
+
 var deferredPrompt;
 
 if (!window.Promise) {
   window.Promise = Promise;
 }
 
-if ("serviceWorker" in navigator) {
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register("/sw.js")
-    .then(function() {
-      console.log("Service worker registered!");
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
     })
     .catch(function(err) {
       console.log(err);
     });
 }
 
-window.addEventListener("beforeinstallprompt", function(event) {
-  console.log("beforeinstallprompt fired");
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
   event.preventDefault();
   deferredPrompt = event;
   return false;
 });
 
-// var promise = new Promise(function(resolve, reject) {
-//   setTimeout(function() {
-//     //resolve('This is executed once the timer is done!');
-//     reject({ code: 500, message: "An error occurred!" });
-//     //console.log('This is executed once the timer is done!');
-//   }, 3000);
-// });
+var promise = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    //resolve('This is executed once the timer is done!');
+    reject({code: 500, message: 'An error occurred!'});
+    //console.log('This is executed once the timer is done!');
+  }, 3000);
+});
 
-console.log("This is executed right after setTimeout()");
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://httpbin.org/ip');
+xhr.responseType = 'json';
 
-fetch("https://httpbin.org/ip")
+xhr.onload = function() {
+  console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+  console.log('Error!');
+};
+
+xhr.send();
+
+fetch('https://httpbin.org/ip')
   .then(function(response) {
     console.log(response);
     return response.json();
@@ -44,13 +57,14 @@ fetch("https://httpbin.org/ip")
     console.log(err);
   });
 
-fetch("https://httpbin.org/post", {
-  method: "POST",
+fetch('https://httpbin.org/post', {
+  method: 'POST',
   headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json"
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
   },
-  body: JSON.stringify({ message: "Does this work?" })
+  mode: 'cors',
+  body: JSON.stringify({message: 'Does this work?'})
 })
   .then(function(response) {
     console.log(response);
@@ -62,3 +76,21 @@ fetch("https://httpbin.org/post", {
   .catch(function(err) {
     console.log(err);
   });
+
+// promise.then(function(text) {
+//   return text;
+// }, function(err) {
+//   console.log(err.code, err.message)
+// }).then(function(newText) {
+//   console.log(newText);
+// });
+
+promise.then(function(text) {
+  return text;
+}).then(function(newText) {
+  console.log(newText);
+}).catch(function(err) {
+  console.log(err.code, err.message);
+});
+
+console.log('This is executed right after setTimeout()');
